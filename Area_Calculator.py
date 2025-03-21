471b40b875b49524a9af02cc65546bf5f21baac1
Dark98 <dark98cooperation@gmail.com>
2025-03-21 10:38:10 +0100

    New calculation system

--- Area_Calculator.py
+++ Area_Calculator.py
@@ -1,23 +1,27 @@
 import time
-from typing import Annotated, NoReturn
-import numpy as np
+import array
+from statistics import median
+from typing import Annotated
 from pynput.mouse import Listener
 import typer
 
 SAMPLE_RATE = 0.01
 GRACE_PERIOD = 5
-input_x = np.array([], dtype=np.uint16)
-input_y = np.array([], dtype=np.uint16)
+inputs_x = array.array('f')
+inputs_y = array.array('f')
+
+screen_width_px: int = 1920
+screen_height_px: int = 1080
 
 
-def on_move(x: int, y: int) -> NoReturn:
+def on_move(x: int, y: int) -> None:
     """Records cursor movements"""
-    global input_x, input_y
-    input_x = np.append(input_x, x)
-    input_y = np.append(input_y, y)
+    global inputs_x, inputs_y, screen_width_px, screen_height_px
+    inputs_x.append(x - (screen_width_px / 2))
+    inputs_y.append(y - (screen_height_px / 2))
 
 
-def record_movements(duration: int) -> NoReturn:
+def record_movements(duration: int) -> None:
     """Records cursor movements for the given duration"""
     with Listener(on_move=on_move):
         time.sleep(GRACE_PERIOD)
@@ -39,12 +43,12 @@ def find_peak_near_extremes(values, min_val, max_val, threshold_percentage=5):
     near_max = near_max[near_max >= 0]
 
     if len(near_min) > 0:
-        min_peak = np.bincount(near_min.astype(int)).argmax()
+        min_peak = inputs_x.bincount(near_min.astype(int)).argmax()
     else:
         min_peak = int(min_val)
 
     if len(near_max) > 0:
-        max_peak = np.bincount(near_max.astype(int)).argmax()
+        max_peak = inputs_y.bincount(near_max.astype(int)).argmax()
     else:
         max_peak = int(max_val)
 
@@ -52,32 +56,69 @@ def find_peak_near_extremes(values, min_val, max_val, threshold_percentage=5):
 
 
 def analyze_data(
-    tablet_width_mm: int,
-    tablet_height_mm: int,
-    innergameplay_width_px: int,
-    innergameplay_height_px: int,
+    current_area_size: float,
+    playfield_size_px: int,
+    inputs: array
 ):
     """Analyzes the movement data and finds dimensions & peak points"""
 
-    # Remove soft outliers (0.01 - 99.99 percentiles)
-    x_1, x_9 = np.percentile(input_x, [0.01, 99.99])
-    y_1, y_9 = np.percentile(input_y, [0.01, 99.99])
+    playfield_inputs = array.array('f')
 
-    width_px_filtered = x_9 - x_1
-    height_px_filtered = y_9 - y_1
+    for input in inputs:
+        if input < playfield_size_px * 0.6:
+            playfield_inputs.append(input)
 
-    # Convert to mm
-    width_mmC_filtered = (width_px_filtered * tablet_width_mm) / innergameplay_width_px
-    height_mmC_filtered = (height_px_filtered * tablet_height_mm) / innergameplay_height_px
+    max_input = max(playfield_inputs)
+    min_input = min(playfield_inputs)
 
-    # Find peak usage near the filtered extremes
-    x_min_peak, x_max_peak = find_peak_near_extremes(input_x, x_1, x_9)
-    y_min_peak, y_max_peak = find_peak_near_extremes(input_y, y_1, y_9)
+    inputs9 = array.array('f')
+    inputs1 = array.array('f')
+    
+    for input in playfield_inputs:
+        if input > max_input * 0.9:
+            inputs9.append(input)
 
-    x_distance_px = x_max_peak - x_min_peak
-    y_distance_px = y_max_peak - y_min_peak
-    x_distance_mm = (x_distance_px * tablet_width_mm) / innergameplay_width_px
-    y_distance_mm = (y_distance_px * tablet_height_mm) / innergameplay_height_px
+        if input < min_input * 0.9:
+            inputs1.append(input * -1)
+
+    median_9: float = median(inputs9)
+    median_1: float = median(inputs1)
+
+    played_area = median_9 + median_1
+
+    if played_area - (playfield_size_px) < 0.1 and played_area - (playfield_size_px) > -0.1:
+        return current_area_size
+    
+    corrected_area: float = current_area_size * played_area / (playfield_size_px)
+    
+    return corrected_area
+
+
+def main(
+    screen_height_px: Annotated[
+        int, typer.Option(prompt="Enter your screen height in pixels", min=600)
+    ],
+    tablet_width_mm: Annotated[
+        float,
+        typer.Option(prompt="Enter your full active tablet area width in mm", min=1.0),
+    ],
+    tablet_height_mm: Annotated[
+        float,
+        typer.Option(prompt="Enter your full active tablet area height in mm", min=1.0),
+    ],
+    duration: Annotated[
+        int, typer.Option(prompt="Enter map duration in seconds", min=10)
+    ],
+):
+    playfield_height_px = screen_height_px * 0.8
+    playfield_width_px = playfield_height_px / 3 * 4
+    typer.confirm("Press Enter to start recording", default=True)
+
+    record_movements(duration)
+
+    x_distance_mm = analyze_data(tablet_width_mm, playfield_width_px, inputs_x)
+
+    y_distance_mm = analyze_data(tablet_height_mm, playfield_height_px, inputs_y)
 
     typer.echo("\n==== RESULTS ====")
     # typer.echo(
@@ -89,42 +130,9 @@ def analyze_data(
         f" {x_distance_mm:.2f} x {y_distance_mm:.2f} mm"
     )
 
-
-def main(
-    screen_width_px: Annotated[
-        int, typer.Option(prompt="Enter your screen width in pixels", min=800)
-    ],
-    screen_height_px: Annotated[
-        int, typer.Option(prompt="Enter your screen height in pixels", min=600)
-    ],
-    tablet_width_mm: Annotated[
-        int,
-        typer.Option(prompt="Enter your full active tablet area width in mm", min=1),
-    ],
-    tablet_height_mm: Annotated[
-        int,
-        typer.Option(prompt="Enter your full active tablet area height in mm", min=1),
-    ],
-    duration: Annotated[
-        int, typer.Option(prompt="Enter map duration in seconds", min=10)
-    ],
-):
-    innergameplay_height_px = int((864 / 1080) * screen_height_px)
-    innergameplay_width_px = int((1152 / 1920) * screen_width_px)
-    typer.confirm("Press Enter to start recording", default=True)
-
-    record_movements(duration)
-    analyze_data(
-        tablet_width_mm=tablet_width_mm,
-        tablet_height_mm=tablet_height_mm,
-        innergameplay_width_px=innergameplay_width_px,
-        innergameplay_height_px=innergameplay_height_px,
-    )
-
     again = typer.confirm("Want to record again?", default=True)
     if again:
         return main(
-            screen_width_px,
             screen_height_px,
             tablet_width_mm,
             tablet_height_mm,
